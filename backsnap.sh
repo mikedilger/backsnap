997338a80a79967c9029e1a429cd2595f764a467
Mike Dilger <mike@efx.co.nz>
2011-07-20 12:59:47 +1200

    Copyright and license GPL v3.0

--- backsnap.sh
+++ backsnap.sh
@@ -1,6 +1,27 @@
 #!/bin/sh
+
 # Backsnap:   Pushes or pulls a backup snapshot to/from a directory to backup
-VERSION=0.3
+#
+# This file is part of Backsnap.
+#
+# This code is Copyright (c) Optimal Computing Limited of New Zealand, 2011.
+# mike@optimalcomputing.co.nz  Michael Dilger
+#
+# Backsnap is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# Backsnap is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+
+#VERSION=0.3
 #
 # Usage:  backsnap <dest> <src> [<src> ...]
 #
